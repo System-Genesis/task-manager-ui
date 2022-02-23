4e260e98d253c05bef045241f7aed9c8d28e98df
yoniZangolsaz <yoniza770@gmail.com>
2022-02-23 16:58:19 +0200

    change

--- src/components/Area.jsx
+++ src/components/Area.jsx
@@ -3,9 +3,11 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 
-
 const Area = () => {
-  const a = [{pageNum: 1, title: 'Post'}, {pageNum: 2, title: 'Get'}]; 
+  const areas = [
+    { pageNum: 1, title: 'Post' },
+    { pageNum: 2, title: 'Get' },
+  ];
   return (
     <Box
       sx={{
@@ -15,12 +17,24 @@ const Area = () => {
         justifyContent: 'center',
       }}
     >
-      {a.map((obj) => (
+      {areas.map((obj) => (
         <Paper
           elevation={10}
           variant='elevation'
-          sx={{ minWidth: '28vw', height: '70vh', m: 2, borderRadius: '20px' }}
-        ><Typography variant='h5' sx={{textAlign: 'center', pt: 2, fontWeight: 'bold', }}>{obj.title}</Typography></Paper>
+          sx={{ minWidth: '28vw', height: '70vh', m: 3, borderRadius: '20px' }}
+        >
+          <Typography
+            variant='h4'
+            sx={{
+              textAlign: 'center',
+              pt: 2,
+              fontWeight: 'bold',
+              color: 'primary.light',
+            }}
+          >
+            {obj.title}
+          </Typography>
+        </Paper>
       ))}
     </Box>
   );
